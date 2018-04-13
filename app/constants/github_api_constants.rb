8cb11d3012458ba927b09f028978b0fc4728aaf4
Anirudh <anirudh.gupta@yatra.com>
2018-04-13 16:31:25 +0530

    correcting indentation

--- app/constants/github_api_constants.rb
+++ app/constants/github_api_constants.rb
@@ -5,6 +5,6 @@ class GithubApiConstants
   # export GITHUB_ACCESS_TOKEN = ''
   GITHUB_ACCESS_TOKEN = ENV['GITHUB_ACCESS_TOKEN'] || ''
 
-   GITHUB_API_LIMIT_REACHED_MESSAGE = "Github API Limit Reached. Please export GITHUB_ACCESS_TOKEN to make authenticated requests."
+  GITHUB_API_LIMIT_REACHED_MESSAGE = "Github API Limit Reached. Please export GITHUB_ACCESS_TOKEN to make authenticated requests."
 
 end
